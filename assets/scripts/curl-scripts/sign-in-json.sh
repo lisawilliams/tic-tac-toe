1473b5fad54006469077b0522140bd3e148cab24
lisawilliams <lisa@placeblogger.com>
2017-05-16 12:15:07 -0400

    Got HTML signup form working in Bootstrap modal.
    Also added some comments to the shell scripts for sign in and sign up.

--- assets/scripts/curl-scripts/sign-in-json.sh
+++ assets/scripts/curl-scripts/sign-in-json.sh
@@ -16,5 +16,20 @@
     }'
   echo
 
-  # returns user object. Note token for this user:
-# {"user":{"id":43,"email":"heather@heatheradels.com","token":"BAhJIiU5NGM0MzU0MzMwYWQ1MWY5MTNhNWU0ZjdjNzVhY2NmOAY6BkVG--72eb17711e591f6bfd6c5d27382db9f633501e91"}}~/wdi/projects/tic-tac-toe (master)
+  # this works and returns:
+  # HTTP/1.1 200 OK
+  # Server: nginx/1.4.6 (Ubuntu)
+  # Date: Tue, 16 May 2017 16:09:34 GMT
+  # Content-Type: application/json; charset=utf-8
+  # Transfer-Encoding: chunked
+  # Connection: keep-alive
+  # X-Frame-Options: SAMEORIGIN
+  # X-XSS-Protection: 1; mode=block
+  # X-Content-Type-Options: nosniff
+  # ETag: W/"7607704708cbd0585c64b7e27eb78f3d"
+  # Cache-Control: max-age=0, private, must-revalidate
+  # X-Request-Id: 42f52a32-93f8-4a55-9c52-55fb3ccd1fe1
+  # X-Runtime: 0.091473
+  # Vary: Origin
+  #
+  # {"user":{"id":43,"email":"heather@heatheradels.com","token":"BAhJIiVmODZhZWJmZDlkNTFmZWNmNGNkMmJjODQ2MjJjNTRmYQY6BkVG--8543063fdb09c458aff36c522f61602a6f5dcfeb"}}~/wdi/projects/tic-tac-toe (development)
--- assets/scripts/curl-scripts/sign-up-json.sh
+++ assets/scripts/curl-scripts/sign-up-json.sh
@@ -19,3 +19,5 @@ echo
 # creates user object and returns it
 # This curl script runs in node and creates a user object
 # this user has a user number of 43
+# note token:  Note token for this user:
+# {"user":{"id":43,"email":"heather@heatheradels.com","token":"BAhJIiU5NGM0MzU0MzMwYWQ1MWY5MTNhNWU0ZjdjNzVhY2NmOAY6BkVG--72eb17711e591f6bfd6c5d27382db9f633501e91"}}~/wdi/projects/tic-tac-toe (master)
