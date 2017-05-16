610e85b7891b8be376da0dfda2f4e805cb0a5b99
lisawilliams <lisa@placeblogger.com>
2017-05-16 15:44:27 -0400

    Finally got form in modal to log a credentials object to the terminal.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,14 +1,34 @@
 'use strict'
+
+// const api = require('./api')
+// const ui = require('./ui')
+
 const getFormFields = require('../../../lib/get-form-fields.js')
 
 const onSignUp = function (event) {
   event.preventDefault()
-  let data = getFormFields(event.target)
-  api.signUp(data)
-  .done(ui.success)
-  .fail(ui.fail)
+  const data = getFormFields(event.target)
+  console.log(data)
+  api.post(data)
+    .then(ui.onAddUserSuccess)
+      .catch(ui.onAddUserFailure)
+
+  // const input = $('#credentials[email]').val()
+  // console.log(input)
+
+//  api.signUp(data)
+//  .done(ui.success)
+//  .fail(ui.fail)
+}
+
+const addHandlers = () => {
+  $('#sign-up-form').on('submit', onSignUp)
+  // $('#sign-in').on('submit', onSignIn);
+  // $('#sign-out').on('submit', onSignOut);
+//  $('#change-password').on('submit', onChangePassword);
 }
 
 module.exports = {
-  onSignUp
+  onSignUp,
+  addHandlers
 }
--- index.js
+++ index.js
@@ -3,9 +3,16 @@
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
+const events = require('./assets/scripts/auth/events.js')
+
 // load manifests
 // scripts
 require('./assets/scripts/index.js')
+require('./assets/scripts/auth/events.js')
 
 // styles
 require('./assets/styles/index.scss')
+
+module.exports = {
+  events
+}
