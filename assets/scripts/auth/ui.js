6568676e6e03d36f6cc6f46eaf83ede91b25a604
lisawilliams <lisa@placeblogger.com>
2017-05-16 22:30:54 -0400

    Set up file structure game scripts and added auth forms
    Set up file structure for /game just the way I set it up for /auth
    and added starter files.
    
    Added additional forms to index.html in preparation for working scripts
    to hook into them tomorrow.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -32,5 +32,7 @@ module.exports = {
   failure,
   success,
   onAddUserSuccess,
-  onAddUserFailure
+  onAddUserFailure,
+  onSignInSuccess,
+  onSignInFailure
 }
---
+++ assets/scripts/game/events.js
@@ -0,0 +1,4 @@
+'use strict'
+
+ // this will probably look like /auth/events.js
+ // start there
